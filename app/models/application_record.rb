86ef41f28c962fbbdd36d2f95feca6226960034c
folkens2 <tfolkens@chicagobooth.edu>
2017-05-17 11:24:57 -0500

    moved validations to 'actors.rb' and 'character.rb' models rather than 'application_record.rb'

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,19 +1,4 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
-  # validations
-  # Character:
-  #  - movie_id: must be present
-
-  validates :movie_id, presence: true
-
-  #  - actor_id: must be present
-  #  - name: no rules
-  #
-  # Actor:
-  #  - name: must be present; must be unique in combination with dob
-  #  - dob: no rules
-  #  - bio: no rules
-  #  - image_url: no rules
-
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,2 +1,13 @@
 class Character < ApplicationRecord
+
+  # validations
+  # Character:
+  #  - movie_id: must be present
+  validates :movie_id, :presence => true
+
+  #  - actor_id: must be present
+  validates :actor_id, :presence => true
+
+  #  - name: no rules
+
 end
