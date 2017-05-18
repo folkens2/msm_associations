3d8a9c9dab8b26417266337981ac3f61e3620e9b
folkens2 <tfolkens@chicagobooth.edu>
2017-05-18 09:08:29 -0500

    creating another belongs_to

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -16,4 +16,6 @@ class Movie < ApplicationRecord
   # Adding a method for finding a director
   belongs_to(:director, :class_name => "Director", :foreign_key => "director_id")
 
+  has_many(:characters, :class_name => "Character", :foreign_key => "movie_id")
+
 end
